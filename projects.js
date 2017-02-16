2bee18d7e4029c1eaa5b375510fe771a21fecccf
nickroberts404 <nickroberts404@gmail.com>
2017-02-16 17:26:58 -0600

    Added color versions of icons, as well as prop to choose between the two

--- projects.js
+++ projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IconElements, IconThoughts, IconLocation } from './components/Icons'
+const color = true;
 
 const projects = [
 	{
@@ -7,21 +8,21 @@ const projects = [
 		description: 'This is a very futuristic project, it may take a couple years to catch on.',
 		projectLink: 'http://elementcalc.me/',
 		pageLink: null,
-		icon: <IconElements />
+		icon: <IconElements color={color}/>
 	},
 	{
 		title: 'Thoughtjar',
 		description: 'This is a very futuristic project, it may take a couple years to catch on.',
 		projectLink: 'https://nickroberts404.github.io/thoughtjar/',
 		pageLink: null,
-		icon: <IconThoughts />,
+		icon: <IconThoughts color={color}/>,
 	},
 	{
 		title: 'Location Data Mocker',
 		description: 'This is a very futuristic project, it may take a couple years to catch on.',
 		projectLink: 'https://github.com/nickroberts404/location-data-generator',
 		pageLink: null,
-		icon: <IconLocation />
+		icon: <IconLocation color={color}/>
 	}
 ]
 export default projects;
