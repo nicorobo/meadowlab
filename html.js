e63dc7ee53801f546e48491505bd5d2753bd5c8e
nickroberts404 <nickroberts404@gmail.com>
2017-02-16 22:40:03 -0600

    Fixed font situation

--- html.js
+++ html.js
@@ -31,6 +31,7 @@ module.exports = React.createClass({
           {head.title.toComponent()}
           {head.meta.toComponent()}
           {css}
+          <link href="https://fonts.googleapis.com/css?family=Muli:300,400,900" rel="stylesheet">
         </head>
         <body>
           <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
