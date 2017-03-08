978b83c6b3cbcf3547f10221e8bc3edc753d19ef
nickroberts404 <nickroberts404@gmail.com>
2017-03-07 21:53:20 -0600

    Updated project descriptions

--- projects.js
+++ projects.js
@@ -5,7 +5,7 @@ const color = false;
 const projects = [
 	{
 		title: 'Element Calculator',
-		description: 'An interactive, responsive periodic table that functions as a molecular mass calculator.',
+		description: 'A molecular mass/mass percentage calculator in the form of a periodic table. It allows you to add and subtract elements by clicking on them, or by entering the formula.',
 		projectLink: 'http://elementcalc.me/',
 		pageLink: null,
 		gitLink: 'https://github.com/nickroberts404/element-calc',
@@ -13,15 +13,15 @@ const projects = [
 	},
 	{
 		title: 'Thoughtjar',
-		description: 'A tool for organizing thoughts and ideas into categories, storing them in the browser for offline use.',
-		projectLink: 'http://thoughtjar.co/',
+		description: 'A web app for organizing thoughts and ideas into categories. You can add new categories, assign them colors, and use them to filter thoughts. Utilizes service workers and the localStorage API to allow offline use.',
+		projectLink: 'https://thoughtjar.co/',
 		pageLink: null,
 		gitLink: 'https://github.com/nickroberts404/thoughtjar',
 		icon: <IconThoughts color={color}/>,
 	},
 	{
 		title: 'Dirbook',
-		description: 'A CLI that enables users to save, open, and copy directories. Dirbook can also manage aliases for saved directories for quick access.',
+		description: 'A CLI that enables users to save, open, and copy directories. Dirbook can optionaly manage aliases for saved directories in <code>.bashrc</code>. I use this a lot for saving starter projects to copy later, and for hopping around between my projects quickly.',
 		projectLink: 'https://www.npmjs.com/package/dirbook',
 		pageLink: null,
 		gitLink: 'https://github.com/nickroberts404/dirbook',
@@ -29,7 +29,7 @@ const projects = [
 	},
 	{
 		title: 'Mirageo',
-		description: 'A library for creating mock location data, optionally within a bounding box/polygon.',
+		description: 'A library for creating mock location data, optionally within a bounding box/polygon. <a href="https://github.com/nickroberts404/mirageo-server"><b>mirageo-server</b></a> spins up a local server that exposes the data via HTTP and provides a map interface for location visualization and bound manipulation.',
 		projectLink: 'https://www.npmjs.com/package/mirageo',
 		pageLink: null,
 		gitLink: 'https://github.com/nickroberts404/mirageo',
@@ -37,7 +37,7 @@ const projects = [
 	},
 	{
 		title: 'Mendeleev',
-		description: 'A small library for building compounds and working with element data.',
+		description: 'A small library for building compounds and working with element data. <b>PeriodicTable</b> acts as a wrapper around a JSON file detailing all of the elements and their charecteristics. <b>Compound</b> is responsible for parsing and formatting compound notation, as well as performing the mass/mass percentage calculations.',
 		projectLink: 'https://www.npmjs.com/package/mendeleev',
 		pageLink: null,
 		gitLink: 'https://github.com/nickroberts404/Mendeleev',
